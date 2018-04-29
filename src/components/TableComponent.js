a51414ff89facec21bf20988e62ad2e664918ce1
Mungujakisa Nickson <nikesonic5@gmail.com>
2018-04-29 21:34:26 +0300

    Completed circuit grid and table component

--- src/components/TableComponent.js
+++ src/components/TableComponent.js
@@ -3,6 +3,7 @@
  */
 import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
+import * as API from "../utilities/API";
 
 const styles = {
     paper: {
@@ -15,123 +16,202 @@ const styles = {
     }
 };
 
+const MOCK = true;
+
 class TableComponent extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            actions: [
-                {
-                    action: "action",
-                    v1: "line 1 voltage",
-                    v2: "line 2 voltage",
-                    v3: "line 3 voltage",
-                    c1: "line 1 current",
-                    c2: "line 2 current",
-                    c3: "line 3 current",
-                    time: "Time"
-                },
-                {
-                    action: "action 2",
-                    v1: "line 1 voltage",
-                    v2: "line 2 voltage",
-                    v3: "line 3 voltage",
-                    c1: "line 1 current",
-                    c2: "line 2 current",
-                    c3: "line 3 current",
-                    time: "Time 2"
-                }
-            ]
-        };
-
-        this.getData = this.getData.bind(this);
-        this.getTableRows = this.getTableRows.bind(this);
-        this.mockData = this.mockData.bind(this);
-    }
-
-    mockData() {
-        return {
-            action: "action",
-            v1: "line 1 voltage",
-            v2: "line 2 voltage",
-            v3: "line 3 voltage",
-            c1: "line 1 current",
-            c2: "line 2 current",
-            c3: "line 3 current",
-            time: "Time"
+            entries: this.getFirstData()
         };
     }
 
-    getData(){
-        return [
-            this.mockData(),
-            this.mockData(),
-            this.mockData(),
-            this.mockData(),
-            this.mockData(),
-        ];
+    componentDidMount() {
+        this.firstLoad();
     }
 
-    getTableRows(){
-        this.getData().map(entry =>{
-            return (
+    displayEntries = (received_entries) => {
+        let items = [];
+        received_entries.map(entry => {
+            items.push(
                 <tr>
-                    <td>{entry.action}</td>
-                    <td>{entry.v1}</td>
-                    <td>{entry.v2}</td>
-                    <td>{entry.v3}</td>
-                    <td>{entry.c1}</td>
-                    <td>{entry.c2}</td>
-                    <td>{entry.c3}</td>
+                    <td>{entry.line_1.voltage}</td>
+                    <td>{entry.line_2.voltage}</td>
+                    <td>{entry.line_3.voltage}</td>
+                    <td>{entry.line_4.voltage}</td>
+                    <td>{entry.line_5.voltage}</td>
+                    <td>{entry.line_1.current}</td>
+                    <td>{entry.line_2.current}</td>
+                    <td>{entry.line_3.current}</td>
+                    <td>{entry.line_4.current}</td>
+                    <td>{entry.line_5.current}</td>
+                    <td>{entry.power}</td>
                     <td>{entry.time}</td>
                 </tr>
-            );
+            )
         });
-    }
+        return items;
+    };
 
     render() {
+        let entries = this.displayEntries(this.state.entries);
+
         return (
             <div>
                 <Paper elevation={5} style={styles.paper}>
                     <div>
                         <table style={Object.assign({}, styles.table, styles.th)}>
                             <tr>
-                                <th>Action</th>
-                                <th colSpan="3">Voltage</th>
-                                <th colSpan="3">Currents</th>
+                                <th colSpan="5">Voltage</th>
+                                <th colSpan="5">Currents</th>
+                                <th></th>
                                 <th>Time</th>
                             </tr>
                             <tr>
-                                <td></td>
                                 <th>Line 1</th>
                                 <th>Line 2</th>
                                 <th>Line 3</th>
+                                <th>Line 4</th>
+                                <th>Line 5</th>
                                 <th>Line 1</th>
                                 <th>Line 2</th>
                                 <th>Line 3</th>
+                                <th>Line 4</th>
+                                <th>Line 5</th>
+                                <th>Power</th>
                                 <td></td>
                             </tr>
-                            {
-                                this.state.actions.map(entry =>{
-                                    return (
-                                        <tr>
-                                            <td>{entry.action}</td>
-                                            <td>{entry.v1}</td>
-                                            <td>{entry.v2}</td>
-                                            <td>{entry.v3}</td>
-                                            <td>{entry.c1}</td>
-                                            <td>{entry.c2}</td>
-                                            <td>{entry.c3}</td>
-                                            <td>{entry.time}</td>
-                                        </tr>
-                                    );
-                                })
-                            }
+                            {entries}
                         </table>
                     </div>
                 </Paper>
             </div>
         );
     }
+
+    firstLoad = () => {
+        console.log('First load of table');
+        this.everyFewSeconds();
+    };
+
+    everyFewSeconds = () => {
+        const INTERVAL = 7000;
+        setInterval(() => {
+            console.log('Refreshing table state');
+            /**/
+            API.readData('development/hardware-states', 'line-states').then((result) => {
+                let old_values = this.state.entries;
+                let received_values = this.formatLineStates(result);
+                old_values.unshift(received_values);
+                this.updateEntries(old_values)
+            }).catch((error) => {
+                console.log('Error below');
+                console.log(error);
+            })
+            /**/
+        }, INTERVAL)
+    };
+
+    updateEntries = (received_entries) => {
+        this.setState({
+            entries: received_entries
+        });
+    };
+
+    formatLineStates = (raw) => {
+        if (MOCK) {
+            let line_states = {
+                line_1: {
+                    current: this.getRandomCurrent(),
+                    voltage: this.getRandomVoltage()
+                },
+                line_2: {
+                    current: this.getRandomCurrent(),
+                    voltage: this.getRandomVoltage()
+                },
+                line_3: {
+                    current: this.getRandomCurrent(),
+                    voltage: this.getRandomVoltage()
+                },
+                line_4: {
+                    current: this.getRandomCurrent(),
+                    voltage: this.getRandomVoltage()
+                },
+                line_5: {
+                    current: this.getRandomCurrent(),
+                    voltage: this.getRandomVoltage()
+                },
+                power: this.getRandomPower(),
+                time: this.getRandomTime()
+            };
+            return line_states;
+        }
+        console.log('Raw line results below');
+        console.log(raw);
+        return raw;
+    };
+
+    getFirstData = () => {
+        if (MOCK) {
+            return [
+                this.getMockData(),
+                this.getMockData(),
+                this.getMockData()
+            ]
+        } else {
+            return [
+                this.getMockData()
+            ]
+        }
+    };
+
+    getMockData = () => {
+        let line_states = {
+            line_1: {
+                current: this.getRandomCurrent(),
+                voltage: this.getRandomVoltage()
+            },
+            line_2: {
+                current: this.getRandomCurrent(),
+                voltage: this.getRandomVoltage()
+            },
+            line_3: {
+                current: this.getRandomCurrent(),
+                voltage: this.getRandomVoltage()
+            },
+            line_4: {
+                current: this.getRandomCurrent(),
+                voltage: this.getRandomVoltage()
+            },
+            line_5: {
+                current: this.getRandomCurrent(),
+                voltage: this.getRandomVoltage()
+            },
+            power: this.getRandomPower(),
+            time: this.getRandomTime()
+        };
+        return line_states;
+    };
+
+    getRandomTime = () => {
+        return `${this.getRndInteger(0, 23)}:${this.getRndInteger(10, 59)} AM`;
+    };
+
+    getRandomPower = () => {
+        return `${this.getRndInteger(500, 700)} Watts`;
+    };
+
+    getRandomCurrent = () => {
+        return `${this.getRndInteger(200, 300)} A`;
+    };
+
+    getRandomVoltage = () => {
+        return `${this.getRndInteger(110, 240)} V`;
+    };
+
+    getRndInteger = (min, max) => {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
 }
 
 export default TableComponent;
