3cdb9a3fb2cbf29110d3d77004ecd9fc385b676d
Mungujakisa Nickson <nicksonmjk@gmail.com>
2018-11-17 12:39:44 +0300

    added mock code

--- src/utilities/Mock.js
+++ src/utilities/Mock.js
@@ -3,3 +3,33 @@
  * TODO: API mocking code goes here
  */
  
+ /**
+ * Created by nickson on 4/23/2018.
+ * Backend abstraction module
+ */
+
+export function login(email, password) {
+   
+}
+
+
+export function logout(email, password) {
+    
+}
+
+
+export function isLoggedIn() {
+    return true
+}
+
+export function saveData(location, key, value){
+    
+}
+
+export function readData(location, key){
+    
+}
+
+export function updateData(location, key, values){
+    
+}
